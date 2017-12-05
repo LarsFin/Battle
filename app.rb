f2caa9c52a7d948fa1a9e38245622671b3c789cc
Lars <larsfinlay@googlemail.com>
2017-12-05 16:12:17 +0000

    Enter a name on login page

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/battle' do
-    'The battle test works!'
+    @name = params[:name]
+    erb(:battle)
+  end
+
+  post '/battle' do
+    @name = params[:name]
+    erb(:battle)
+  end
+
+  get '/login' do
+    @name = params[:name]
+    erb(:login)
   end
 
   run! if app_file == $0
--- spec/feature_tests/testing_infastructure_spec.rb
+++ spec/feature_tests/testing_infastructure_spec.rb
@@ -1,7 +1,20 @@
 feature 'Testing infrastructure' do
+  before do
+    visit '/login'
+  end
 
   scenario 'Can run app and check page content' do
-    visit('/battle')
-    expect(page).to have_content 'The battle test works!'
+    expect(page).to have_content 'Enter name'
   end
+
+  scenario 'Allows players to fill in their name' do
+    expect(page).to have_field('name')
+  end
+
+  scenario 'Expect those names to show on screen after submitting' do
+    fill_in 'name', with: 'TEST'
+    click_button 'GO'
+    expect(page).to have_content 'PLAYER1: TEST'
+  end
+
 end
