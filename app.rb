20b5dcc3b95db9406527419e5920e74405dd6142
Lars <larsfinlay@googlemail.com>
2017-12-06 14:39:06 +0000

    Made Character Select Screen

--- app.rb
+++ app.rb
@@ -3,19 +3,29 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   enable :sessions
 
-  get '/battle' do
-    @name = params[:name]
-    erb(:battle)
-  end
-
-  post '/battle' do
-    @name = params[:name]
-    erb(:battle)
-  end
-
   get '/login' do
-    @name = params[:name]
     erb(:login)
+  end
+
+  post '/login' do
+    session[:name] = params[:name]
+    redirect '/cha_select'
+  end
+
+  get '/cha_select' do
+    @name = session[:name]
+    erb(:cha_select)
+  end
+
+  post '/cha_select' do
+    session[:char] = params[:char]
+    redirect '/battle'
+  end
+
+  get '/battle' do
+    @name = session[:name]
+    @char = session[:char]
+    erb(:battle)
   end
 
   run! if app_file == $0
--- spec/feature_tests/testing_infastructure_spec.rb
+++ spec/feature_tests/testing_infastructure_spec.rb
@@ -14,7 +14,15 @@ feature 'Testing infrastructure' do
   scenario 'Expect those names to show on screen after submitting' do
     fill_in 'name', with: 'TEST'
     click_button 'GO'
-    expect(page).to have_content 'PLAYER1: TEST'
+    expect(page).to have_content 'PLAYER: TEST'
+  end
+
+  scenario 'Should pick a character and display the choice on the next screen' do
+    fill_in 'name', with: 'TEST'
+    click_button 'GO'
+    choose("char", option: "templeknight")
+    click_button 'GO'
+    expect(page).to have_content 'player: TEST' and have_content 'character: TEMPLE KNIGHT'
   end
 
 end
